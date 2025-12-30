263bb51cdeb5afde16eae7d3c37a2a9f94abb3c3
Papoyy12 <zenzenith07@gmail.com>
2025-12-30 10:27:26 +0700

    menambahkan koneksi kedalam folder kconfig

---
+++ config/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username   = "root";
+$password   = "";
+$db   = "pbl"; 
+
+$koneksi = mysqli_connect($host, $username, $password, $db);
+
+if (!$koneksi) {
+    die("Gagal mengakses Database : " . mysqli_connect_error());
+}
+
+?>
