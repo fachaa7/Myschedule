afeeebface4932267a230f8f4bb50adc393fbf84
nareennaa-source <diardina1009@gmail.com>
2025-12-30 21:16:29 +0700

    update

--- process/manajemen.php
+++ process/manajemen.php
@@ -89,7 +89,7 @@ $roleAdmin = $_SESSION['role'];
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/admin.css">
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
